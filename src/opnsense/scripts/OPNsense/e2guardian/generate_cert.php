4407d8ebc70ebf5025a726ff62cd0c367a444713
gunior80 <gunior80@yandex.ru>
2024-05-31 00:14:19 +0300

    append ssl certs support

--- src/opnsense/scripts/OPNsense/e2guardian/generate_cert.php
+++ src/opnsense/scripts/OPNsense/e2guardian/generate_cert.php
@@ -17,12 +17,29 @@ if (is_file('/usr/local/etc/e2guardian/ca.pem.id')) {
     if (!empty($config['ca'])) {
         foreach ($config['ca'] as $ca) {
             if (isset($ca['refid']) && $ca['refid'] == $cert_refid) {
-                $pem_contents = '';
-                $pem_contents .= trim(base64_decode($ca['prv'])) . "\n";
-                $pem_contents .= trim(base64_decode($ca['crt'])) . "\n";
-                $pem_contents .= ca_chain($ca);
+                $ca_key = '';
+                $ca_key .= trim(base64_decode($ca['prv'])) . "\n";
+                $ca_pem = '';
+                $ca_pem .= trim(base64_decode($ca['crt'])) . "\n";
                 echo "certificate generated\n";
-                file_put_contents('/usr/local/etc/e2guardian/ca.pem', $pem_contents);
+                file_put_contents('/usr/local/etc/e2guardian/ca.key', $ca_key);
+                file_put_contents('/usr/local/etc/e2guardian/ca.pem', $ca_pem);
+            }
+        }
+    }
+}
+if (is_file('/usr/local/etc/e2guardian/private.pem.id')) {
+    $cert_refid = trim(file_get_contents('/usr/local/etc/e2guardian/private.pem.id'));
+    if (!empty($config['cert'])) {
+        foreach ($config['cert'] as $cert) {
+            if (isset($cert['refid']) && $cert['refid'] == $cert_refid) {
+                $cert_key = '';
+                $cert_key .= trim(base64_decode($cert['prv'])) . "\n";
+                $cert_pem = '';
+                $cert_pem .= trim(base64_decode($cert['crt'])) . "\n";
+                echo "certificate generated\n";
+                file_put_contents('/usr/local/etc/e2guardian/private.key', $cert_key);
+                file_put_contents('/usr/local/etc/e2guardian/private.pem', $cert_pem);
             }
         }
     }
