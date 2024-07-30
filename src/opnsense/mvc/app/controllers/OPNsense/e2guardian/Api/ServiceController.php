f9315bf8fdb58c2393dcccf40258097f4ccfb53c
Дмитрий Ванюшкин <gunior80@yandex.ru.ru>
2024-07-30 15:38:10 +0300

    deleting files deleted entries

--- src/opnsense/mvc/app/controllers/OPNsense/e2guardian/Api/ServiceController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/e2guardian/Api/ServiceController.php
@@ -4,6 +4,7 @@
 
 namespace OPNsense\e2guardian\Api;
 
+use OPNsense\Core\Backend;
 use OPNsense\Base\ApiMutableServiceControllerBase;
 
 class ServiceController extends ApiMutableServiceControllerBase
@@ -12,5 +13,23 @@ class ServiceController extends ApiMutableServiceControllerBase
     protected static $internalServiceTemplate = 'OPNsense/e2guardian';
     protected static $internalServiceEnabled = 'enabled';
     protected static $internalServiceName = 'e2guardian';
+
+    /**
+     * @return array
+     */
+    public function reconfigureAction()
+    {
+        if (!$this->request->isPost()) {
+            return ['result' => 'failed'];
+        }
+
+        $this->sessionClose();
+        $backend = new Backend();
+        $backend->configdRun('template cleanup ' . escapeshellarg(static::$internalServiceTemplate));
+        $backend->configdRun('template reload ' . escapeshellarg(static::$internalServiceTemplate));
+
+        return ['result' => 'ok'];
+    }
+
 
 }
